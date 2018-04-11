a796802bac6a7bd53eef242d7e25efbac59a1f2b
Nicolás Miari <nicolasmiari@gmail.com>
2018-04-11 20:50:21 +0900

    Add dependency

---
+++ code/B/B/B.h
@@ -0,0 +1,19 @@
+//
+//  B.h
+//  B
+//
+//  Created by Nicolás Miari on 2018/04/11.
+//  Copyright © 2018 Nicolás Miari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for B.
+FOUNDATION_EXPORT double BVersionNumber;
+
+//! Project version string for B.
+FOUNDATION_EXPORT const unsigned char BVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <B/PublicHeader.h>
+
+
---
+++ code/B/B/MyClass.swift
@@ -0,0 +1,19 @@
+//
+//  MyClass.swift
+//  B
+//
+//  Created by Nicolás Miari on 2018/04/11.
+//  Copyright © 2018 Nicolás Miari. All rights reserved.
+//
+
+import Foundation
+import C
+
+open class MyClass {
+
+    let myProperty: SampleClass
+
+    public init() {
+        self.myProperty = SampleClass()
+    }
+}
